6e260522ac77a41fa293cb561684b15f63e77092
Asvprocky <cytron1225@naver.com>
2022-08-08 17:05:00 +0900

    init 0.0.1

---
+++ src/integrated/App.jsx
@@ -0,0 +1,78 @@
+import { List, Paper } from "@mui/material";
+import { Container } from "@mui/system";
+import { useEffect } from "react";
+import { useState } from "react";
+import AddTodo from "./AddTodo";
+import Todo from "./Todo";
+import axios from 'axios';
+import { API_BASE_URL } from './api-config'
+
+
+function App() {
+  // Todo 목록리스트
+  const [items, setItems] = useState([]);
+
+
+  //Todo 목록 가져오기 (GET 요청)
+  useEffect(() => {
+    axios({
+      method: 'get',
+      url: API_BASE_URL + '/todo',
+    }).then((response) => {
+      setItems(response.data.resList);
+    });
+  }, []);
+
+  // Todo 생성 (POST 요청)
+  const addItem = (item) => {
+    axios({
+      method: 'post',
+      url: API_BASE_URL + '/todo',
+      data: item,
+    }).then((response) => {
+      setItems(response.data.resList);
+    });
+  }
+
+  // Todo 수정 (PUT 요청)
+  const editItem = (item) => {
+    axios({
+      method: 'put',
+      url: API_BASE_URL + '/todo',
+      data: item,
+    }).then((response) => {
+      setItems(response.data.resList)
+    });
+  }
+
+  //Todo 삭제
+  const deleteItem = '1';
+  // const deleteItem = (item)=>{
+  //   axios({
+  //     method:'delete',
+  //     url:"http://192.168.0.204:8080" + '/todo',
+  //     data:item
+  //   }).then
+  // }
+
+  return (
+    <div className="App">
+      <Container maxWidth="md">
+        <AddTodo addItem={addItem} />
+        <Paper style={{ margin: 16 }}>
+          <List>
+            {
+              items.map((element, idx) => {
+                return <Todo key={element.id}
+                  item={element} editItem={editItem} deleteItem={deleteItem}
+                />
+              })
+            }
+          </List>
+        </Paper>
+      </Container>
+    </div>
+  );
+}
+
+export default App;
